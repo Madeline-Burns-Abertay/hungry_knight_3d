3c0076784b78f86531cd742c9285c47186289b2f
Madeline-Burns-Abertay <2501892@abertay.ac.uk>
2025-11-11 00:44:56 +0000

    mummy complete for real
    spoke too soon earlier

--- Assets/Script/enemy.cs
+++ Assets/Script/enemy.cs
@@ -6,6 +6,11 @@ public class enemy : MonoBehaviour
 {
     [SerializeField] List<GameObject> food;
     Animator anim;
+    bool isPatrolling;
+    float patrolRadius = 6f;
+    Vector3 destination;
+    const float epsilon = 0.01f;
+    [SerializeField] float speed = 0.5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,7 +20,23 @@ public class enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (!isPatrolling)
+        {
+            Vector2 randomPoint = Random.insideUnitCircle * patrolRadius; // pick a point, any point
+            destination = new Vector3(transform.position.x + randomPoint.x, 0, transform.position.z + randomPoint.y); // turn that point into a point in the game world
+            //Debug.Log(destination);
+            transform.LookAt(destination);
+            isPatrolling = true; // set off
+        }
+        if (Vector3.Distance(transform.position, destination) <= epsilon)
+        {
+            isPatrolling = false; // stop
+        }
+        else
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+        Debug.Log("Is patrolling? " + isPatrolling.ToString() + "; Distance: " + Vector3.Distance(transform.position, destination) + "; Direction: " + transform.rotation);
     }
 
     void OnTriggerEnter(Collider other)
